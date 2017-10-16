95d8d996fa615b15ca1e7dfdb473d5d661249113
Krzysztof Drewniak <krzysdrewniak@gmail.com>
2017-10-16 15:04:10 +0200

    Project is up, mostly

---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+Knuth-Bendix
+============
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+.. only:: html
+
+   Indices and tables
+   ==================
+
+   * :ref:`genindex`
+   * :ref:`modindex`
+   * :ref:`search`
---
+++ knuth_bendix/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'knuth_bendix'
+project = "Knuth-Bendix"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'Knuth-Bendix completion algorithm implementation with matchpy'
+authors = ['Krzysztof Drewniak']
+authors_string = ', '.join(authors)
+emails = ['kdrewniak@utexas.edu']
+license = 'GPL3'
+copyright = '2017 ' + authors_string
+url = 'http://github.com/krzysz00/knuth_bendix'
